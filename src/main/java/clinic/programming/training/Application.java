7cb98a407828f7c7c4ebd86ea30e0124cd0869b2
Mohak Chaudhari <mohak.672@gmail.com>
2021-06-18 12:41:03 +0530

    changed pom.xml and Application.java file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,26 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
 
-    // method main(): ALWAYS the APPLICATION entry point
+    public void print(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        System.out.println("Greetings: "+list.get(0));
+    }
+
     public static void main (String[] args) {
         System.out.println ("Starting Application");
         Application app = new Application();
+        //.gitignore will ignore the maven related folder -> not push them on github
+        //ex. target folder (created by maven)
+
+        app.print();
     }
 }
